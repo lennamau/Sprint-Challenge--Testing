1d1dcb58bbc591ae363bf1551a9ce1a60559a767
Lenna <lenna@lennamau.com>
2019-03-08 12:04:28 -0700

    attempted delete tests

--- api/server.js
+++ api/server.js
@@ -9,10 +9,6 @@ server.get('/', async (req, res) => {
     res.status(200).json({ api: 'running' });
 });
 
-server.get('/games', async (req, res) => {
-    const list = await games.getAll();
-    res.status(200).json(list);
-})
 
 server.post('/games', async (req, res) => {
     const game = req.body;
@@ -31,5 +27,29 @@ server.post('/games', async (req, res) => {
     }
 })
 
+server.get('/games', async (req, res) => {
+    const list = await games.getAll();
+    res.status(200).json(list);
+})
+
+server.delete('/games/:id', async (req, res) => {
+    const { id } = req.params;
+    await games.getGamesById(id)
+        .then(game => {
+            const newGame = game;
+            games.remove(id)
+                .then(response => {
+                    if (response) {
+                        res.status(200).json(newGame)
+                    }
+                    else {
+                        res.status(404).json({message: 'that game is not here'})
+                    }
+                })
+        })
+        .catch(err => {
+            res.status(500).json({error: 'error removing game'})
+        })
+})
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -17,18 +17,20 @@ describe('Server Connection', () => {
 })
 describe('Post Route', () => {
     it('responds with 201', async () => {
-
-        const body = {
-            title: 'Just Dance',
-            genre: 'Dance',
-            releaseDate: 2012
+        const game = {
+            title: 'NBA2K15',
+            genre: 'Sports',
+            releaseDate: 2015
         }
         const response = await request(server)
             .post('/games')
-            .send(body);
+            .send(game);
         expect(response.status).toBe(201);
     })
 
+    afterEach(async () => {
+        await db('games').truncate();
+    })
     it('responds with 405', async () => {
 
         const body = {
@@ -55,4 +57,53 @@ describe('Post Route', () => {
     })
 
 })
+
+describe('Get Route', () => {
+    afterEach(async () => {
+        await db('games').truncate();
+    })
+    it('responds with 200', async () => {
+        const response = await request(server).get('/games')
+        expect(response.status).toBe(200);
+    });
+
+    it('returns array of all games', async  () => {
+        const fifa = {
+            title: 'fifa',
+            genre: 'sports',
+            releaseYear: 2015
+        }
+        const mario = {
+            title: 'mario',
+            genre: 'arcade',
+            releaseYear: 2014
+        }
+        await Games.insert([fifa, mario]);
+        const getGames = await Games.getAll();
+        expect(getGames.length).toBe(2);
+    });
+
+    it('returns empty array when no games', async () => {
+        const response = await request(server).get('/games')
+        expect(response.body).toEqual([]);
+    });
+
+})
+
+describe('Delete route', () => {
+    afterEach(async () => {
+        await db('games').truncate();
+    })
+    it('responds with 200', async () => {
+        const response = await request(server).remove('/games/1')
+        expect(response.status).toBe(200)
+    })
+    it('responds with a 404', async () => {
+        const response = await request(server).remove('/games/20')
+        expect(response.status).toBe(404)
+    })
+  
+})
+
+
 
